17f72fac5de7ad93b04f4174b9dc0dd495f668ba
Trey Dockendorf <treydock@gmail.com>
2015-03-03 13:36:23 -0600

    Add acceptance tests

---
+++ spec/acceptance/setroubleshoot_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper_acceptance'
+
+describe 'setroubleshoot class:' do
+  context 'with default parameters' do
+    it 'should run successfully' do
+      pp = "class { 'setroubleshoot': }"
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe package('setroubleshoot-server') do
+      it { should be_installed }
+    end
+
+    describe file('/etc/setroubleshoot/setroubleshoot.conf') do
+      its(:content) { should match /^recipients_filepath = \/var\/lib\/setroubleshoot\/email_alert_recipients$/ }
+      its(:content) { should match /^smtp_port = 25$/ }
+      its(:content) { should match /^smtp_host = localhost$/ }
+      its(:content) { should match /^from_address = SELinux_Troubleshoot$/ }
+    end
+  end
+end
